bab25bcd4a8f06bf780cff78050ac200d2c9d60e
Kelokc <kelokchan@gmailc.om>
2017-10-31 17:45:38 +1000

    test nlp library

--- CAB432/app.js
+++ CAB432/app.js
@@ -19,7 +19,8 @@ const expressStatusMonitor = require('express-status-monitor');
 const sass = require('node-sass-middleware');
 const request = require("request");
 const Twit = require('twit');
-const Twitter = require('twitter');
+const analyze = require('Sentimental').analyze;
+const fs = require("fs");
 
 /**
  * Load environment variables from .env file, where API keys and passwords are configured.
@@ -61,12 +62,12 @@ app.use(expressValidator());
 app.use(session({
   resave: true,
   saveUninitialized: true,
-  secret: process.env.SESSION_SECRET,
-  store: new MongoStore({
-    url: process.env.MONGODB_URI || process.env.MONGOLAB_URI,
-    autoReconnect: true,
-    clear_interval: 3600
-  })
+  secret: process.env.SESSION_SECRET
+  // store: new MongoStore({
+  //   url: process.env.MONGODB_URI || process.env.MONGOLAB_URI,
+  //   autoReconnect: true,
+  //   clear_interval: 3600
+  // })
 }));
 app.use(passport.initialize());
 app.use(passport.session());
@@ -94,35 +95,65 @@ app.get('/', (req, res) => {
 
 app.get('/home', homeController.index);
 
+var tweets = [];
+
+/**
+ * Endpoint for retrieving tweets used for NLP in the backend
+ * Tweets limit is based on the user-selected value
+ */
 app.get('/tweets', (req, res) => {
   let keywords = req.query.keywords;
   let limit = req.query.limit;
-  var tweets = [];
+  tweets = [];
 
   if (keywords.length > 0)
-    stream = T.stream('statuses/filter', { track: keywords });
+    stream = T.stream('statuses/filter', { track: keywords, language: 'en' });
   else
-    stream = T.stream('statuses/sample');
+    stream = T.stream('statuses/sample', { language: 'en' });
 
   stream.on('tweet', (tweet) => {
-    tweets.unshift(tweet);
-    if (tweets.length == limit) {
-      stream.stop();
-      req.flash('success', { msg: 'Tweets retrieved' });
-      res.render('tweets', { title: 'Tweets', keywords: keywords, tweets: tweets });
+    if (tweets.length < limit) {
+      tweets.push(tweet);
+      console.log('Loaded ' + tweets.length + ' of ' + limit);
     }
   });
+
+  // Stop the request after specified seconds
+  setTimeout(() => {
+    stream.stop();
+    req.flash('success', { msg: tweets.length + ' tweets retrieved' });
+    res.render('tweets', { title: 'Tweets', keywords: keywords, tweets: tweets });
+  }, 5 * 1000);
 });
 
-// app.get('/iss', (req, res) => {
-//   var url = "http://api.open-notify.org/iss-now.json";
-//   request(url, (err, response, body) => {
-//       if (!err && response.statusCode == 200){
-//          var data = JSON.parse(body);
-//          res.send(data);
-//       }
-//   });
-// });
+app.get('/test', (req, res) => {
+  console.log(tweets[0]);
+  let result = [];
+
+  let sample = [];
+
+  var texts = fs.readFileSync("public/data/pos.txt").toString().split("\n");
+  var texts2 = fs.readFileSync("public/data/neg.txt").toString().split("\n");
+
+  tweets.forEach(function(tweet) {
+    sample.push(tweet.text);
+  }, this);
+
+  
+  for (i in texts) {
+    sample.push(texts[i]);
+  }
+  
+  for (j in texts2) {
+    sample.push(texts2[j]);
+  }
+  
+  sample.forEach((data) => {
+    result.unshift(analyze(data));
+  }, this);
+  
+  res.json(result);  
+});
 
 /**
  * Error Handler.
